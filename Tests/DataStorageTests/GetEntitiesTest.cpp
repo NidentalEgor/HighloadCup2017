80a520d667e68a9d68228de5b8dde5f2cb74bc72
Egor <nidentalegor@gmail.com>
2017-10-08 03:29:29 +0700

    Added tests. Added time measurement.

--- Tests/DataStorageTests/GetEntitiesTest.cpp
+++ Tests/DataStorageTests/GetEntitiesTest.cpp
@@ -1,17 +1,38 @@
+#include <chrono>
+#include <iostream>
+
 #include "gtest/gtest.h"
 
 #include "DataStorage.h"
 
 class DataStorageGetEntitiesTest : public testing::Test
 {
+public:
+    DataStorageGetEntitiesTest()
+    {
+        start_time_ = std::chrono::high_resolution_clock::now();
+    }
+
+    ~DataStorageGetEntitiesTest()
+    {
+        end_time_ = std::chrono::high_resolution_clock::now();
+        const auto elapsed_time =
+                std::chrono::duration_cast<std::chrono::nanoseconds>(end_time_ - start_time_);
+        std::cout << testing::UnitTest::GetInstance()->current_test_info()->name() <<
+                ": elapsed time = " << elapsed_time.count() <<
+                " nanoseconds" << std::endl << std::endl;
+    }
+
 protected:
-virtual void SetUp()
-{
-    data_storage_.LoadData("/home/egor/Repositories/HighloadCup2017/Tests/DataStorageTests/TestData/ReadData/");
-}
+    virtual void SetUp()
+    {
+        data_storage_.LoadData("/home/egor/Repositories/HighloadCup2017/Tests/DataStorageTests/TestData/ReadData/");
+    }
 
 protected:
     DataStorage data_storage_;
+    std::chrono::high_resolution_clock::time_point start_time_;
+    std::chrono::high_resolution_clock::time_point end_time_;
 };
 
 TEST_F(DataStorageGetEntitiesTest, ReadData)
@@ -45,12 +66,27 @@ TEST_F(DataStorageGetEntitiesTest, GetVisit)
     ASSERT_EQ(*visit, R"({"id":1,"location":22,"user":27,"visited_at":1049447314,"mark":3})");
 }
 
-TEST_F(DataStorageGetEntitiesTest, GetVisitByUserId)
+TEST_F(DataStorageGetEntitiesTest, GetVisitByUserIdOneVisit)
 {
     const auto visit = data_storage_.GetVisistsByUserId(27);
 
     ASSERT_NE(visit, nullptr);
     ASSERT_EQ(*visit, R"({"visits":[{"id":1,"location":22,"user":27,"visited_at":1049447314,"mark":3}]})");
+}
+
+TEST_F(DataStorageGetEntitiesTest, GetVisitByUserIdNoUser)
+{
+    const auto visit = data_storage_.GetVisistsByUserId(28);
+
+    ASSERT_EQ(visit, nullptr);
+}
+
+TEST_F(DataStorageGetEntitiesTest, GetVisitByUserIdWithFromDate)
+{
+    const auto visit = data_storage_.GetVisistsByUserId(27, 1049447315);
+
+    ASSERT_NE(visit, nullptr);
+    ASSERT_EQ(*visit, R"({"visits":[]})");
 }
 
 int main(int argc, char* argv[])
